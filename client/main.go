db37d506ca0980bee021b0c98c9e798d42db07dc
Nish <nishantpatel611@gmail.com>
2018-09-01 14:43:16 -0400

    remove comments

--- client/main.go
+++ client/main.go
@@ -28,10 +28,6 @@ type target struct {
 	Port     string `json`
 }
 
-// func greet(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello World! %s", time.Now())
-// }
-
 func main() {
 	c := flag.String("c", "./config.json", "json config")
 	flag.Parse()
